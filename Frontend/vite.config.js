53ec4ac9564fd942cf03ce9d7c56764409d400ca
JesusAlberto156 <72238331+JesusAlberto156@users.noreply.github.com>
2025-09-03 10:53:09 -0700

    Reparacion de teclado en produccion

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react-simple-keyboard"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-simple-keyboard/, /node_modules/],
+    },
+  },
 })
